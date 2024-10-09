b9bed74ed9953ad5ac3223015b71ecbcfd64c7e9
James Buchanan <jbuchanan@newrelic.com>
2024-10-09 12:06:02 +0100

    added config options

--- visualizations/cumulative-chart/Viz.tsx
+++ visualizations/cumulative-chart/Viz.tsx
@@ -4,11 +4,7 @@ import { AutoSizer } from "nr1";
 import EmptyState from "./components/EmptyState";
 import ExampleComponent from "./components/ExampleComponent";
 
-const Viz = ({ greeting }) => {
-  // return empty state if no config  
-  if (!greeting) {
-    return <EmptyState />;
-  }
+const Viz = ({  }) => {
 
   return (
     <AutoSizer>
---
+++ visualizations/cumulative-chart/components/ExampleComponent.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useProps } from "../context/VizPropsProvider";
+import { NrqlQuery, LineChart } from "nr1";
+
+const ExampleComponent = () => {
+  const vizProps = useProps();
+  const { accountId, query } = vizProps;
+
+  return (
+        <>
+        <NrqlQuery
+          accountIds={[accountId]}
+          query={query}
+        >
+          {({ data }) => {
+            if (data) {
+              // Cumulative sum
+              data.forEach((series)=>{
+                if(series.cumulativeApplied) return;
+                let cumulative = 0;
+                series.data.forEach((d, i) => {
+                  cumulative += d.y;
+                  d.y = cumulative;
+                });
+                series.cumulativeApplied=true;
+              });
+            } 
+            return <LineChart fullWidth fullHeight data={data} />;
+            
+          }}
+        </NrqlQuery>
+        
+        </>
+  );
+};
+
+export default ExampleComponent;
